9ac62fa88ec37e4ea6184e29848e220ca2dfd416
mefercs <mivki890@gmail.com>
2022-09-23 03:55:28 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
 
 function App() {
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Calculator  />    
     </div>
   );
 }
+
+const numbers = ['one','two','three','four','five','six','seven','eight','nine']
+
+const Calculator = () => { 
+
+  const [calc, setCalc] = useState("")
+  const [result, setResult] = useState('')
+
+  const buttonNumbers = numbers.map( (e, index) => <button key={e} onClick={() => addCalc( (index+1).toString())}>{index+1}</button> )
+  const ops = ['-','*','/','+']
+
+  const addCalc = value => { 
+    if( (ops.includes(value) & calc==='' & value!=='-') || (ops.includes( calc.slice(-1)) & ops.includes(value) || (value==='0' & calc==='')) ){
+      return
+    }
+    setCalc( p => p + value )
+    if( ops.includes( value) ){
+      return
+    }
+    setResult(eval(calc + value).toString())
+  }
+
+  const delLast= ()=> { 
+    const newCalc = calc.slice(0,-1)
+    setCalc(newCalc)
+    if( ops.includes(newCalc.slice(-1))) {
+      return
+    }
+    setResult(eval( newCalc ))
+  }
+  const handleResult = () =>{ 
+    const result = eval(calc)
+    setCalc(result.toString())
+    setResult(result)
+  }
+
+
+  return ( <div className='calculator'>
+
+    <p className="display">{ result &&  <>({result})</> } {calc}</p>
+
+    <div className="operators">
+      <button onClick={() => addCalc("-")} className="subtract">-</button>
+      <button onClick={() => addCalc("*")} className="multiply">x</button>
+      <button onClick={() => addCalc("/")} className="divide">/</button>
+      <button onClick={() => addCalc("+")} className="add">+</button>
+      <button onClick={delLast} className="clear">DEL</button>
+    </div>
+    
+    {buttonNumbers}
+
+    <button onClick={() => addCalc("0")} className="zero">0</button>
+    <button className="decimal">.</button>
+    <button onClick={handleResult} className='equals'>=</button>
+
+  </div> )
+}
+
 
 export default App;
